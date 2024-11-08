2a732c92f82385267e08d5457e05a302fe57a67e
Max Dark <maxim.dark@gmail.com>
2024-11-08 19:19:31 +0300

    add base instruction list

--- src/main.cxx
+++ src/main.cxx
@@ -114,6 +114,19 @@ struct OpcodeBase
 };
 static_assert(sizeof(OpcodeBase) == sizeof(opcode_t));
 
+
+/// @see Instruction Length Encoding
+size_t op_size(opcode_t code)
+{
+    if ((0b00000011 & code) != 0b0000011) return 1;
+    if ((0b00011100 & code) != 0b0011100) return 2;
+    if ((0b00111111 & code) == 0b0011111) return 3;
+    if ((0b01111111 & code) == 0b0111111) return 3;
+
+    return 0;
+}
+
+/// @see RV32/64G Instruction Set Listings
 constexpr opcode_t make_opcode(opcode_t i65, opcode_t i42)
 {
     return (i65 << 5) | (i42 << 2) | 0b011;
@@ -122,6 +135,44 @@ static_assert(make_opcode(0b11, 0b111) == 0b011'111'11);
 static_assert(make_opcode(0b11, 0b110) == 0b011'110'11);
 static_assert(make_opcode(0b10, 0b111) == 0b010'111'11);
 static_assert(make_opcode(0b10, 0b110) == 0b010'110'11);
+
+enum OpcodeType: opcode_t
+{
+    LOAD   = make_opcode(0b00, 0b000),
+    STORE  = make_opcode(0b01, 0b000),
+    MADD   = make_opcode(0b10, 0b000),
+    BRANCH = make_opcode(0b11, 0b000),
+
+    LOAD_FP  = make_opcode(0b00, 0b001),
+    STORE_FP = make_opcode(0b01, 0b001),
+    MSUB     = make_opcode(0b10, 0b001),
+    JALR     = make_opcode(0b11, 0b001),
+
+    CUSTOM_0 = make_opcode(0b00, 0b010),
+    CUSTOM_1 = make_opcode(0b01, 0b010),
+    NMSUB = make_opcode(0b01, 0b010),
+    // R_11_010 = make_opcode(0b11, 0b010), // reserved
+
+    MISC_MEM = make_opcode(0b00, 0b011),
+    AMO = make_opcode(0b01, 0b011),
+    NMADD = make_opcode(0b01, 0b011),
+    JAL = make_opcode(0b11, 0b011),
+
+    OP_IMM = make_opcode(0b00, 0b100),
+    OP = make_opcode(0b01, 0b100),
+    OP_FP = make_opcode(0b01, 0b100),
+    SYSTEM = make_opcode(0b11, 0b100),
+
+    AUIPC = make_opcode(0b00, 0b101),
+    LUI = make_opcode(0b01, 0b101),
+    // R_10_101 = make_opcode(0b01, 0b101), // reserved
+    // R_11_101 = make_opcode(0b11, 0b101), // reserved
+
+    OP_IMM_32 = make_opcode(0b00, 0b011),
+    OP_32 = make_opcode(0b01, 0b011),
+    CUSTOM_2 = make_opcode(0b01, 0b011),
+    CUSTOM_3 = make_opcode(0b11, 0b011),
+};
 
 } // namespace opcode
 } // namespace vm
