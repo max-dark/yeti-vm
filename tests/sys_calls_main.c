3bc94d1bac46a0a3ae249a03e884a83b272f693b
Max Dark <maxim.dark@gmail.com>
2024-12-09 11:56:25 +0300

    update syscalls test

--- tests/sys_calls_main.c
+++ tests/sys_calls_main.c
@@ -8,39 +8,79 @@
 
 #include <stdint.h>
 
-extern void sys_exit(void);
-extern int32_t sys_open(const char* filename, uint32_t flags);
-extern int32_t sys_read(uint32_t fd, void * buffer, uint32_t size);
-extern void sys_close(uint32_t fd);
+[[noreturn]]
+void sys_exit(void);
+int32_t sys_open(const char* filename, uint32_t flags);
+int32_t sys_read(uint32_t fd, void * buffer, uint32_t size);
+void sys_close(uint32_t fd);
 
-extern void put_int(int32_t num);
-extern void put_char(char c);
+void put_int(int32_t num);
+void put_char(char c);
 
 #define ELEM_PER_LINE 5
-#define NUM_ELEM 25
+#define NUM_LINES 5
+#define NUM_ELEM (ELEM_PER_LINE * NUM_LINES)
 
 const char fileName[] = "input.txt";
 int32_t buffer[NUM_ELEM];
-int32_t input[NUM_ELEM];
-int32_t result[NUM_ELEM];
+int32_t input[NUM_LINES][NUM_ELEM];
+int32_t result[NUM_LINES][NUM_ELEM];
 
+void app_main();
+
+[[noreturn]]
 void _start()
 {
+    app_main();
+    sys_exit();
+}
 
-    int32_t fd = sys_open(fileName, 0);
+void app_main()
+{
+    int fd = sys_open(fileName, 0);
     sys_read(fd, buffer, sizeof(buffer));
     sys_close(fd);
 
-    for (int idx = 0; idx < NUM_ELEM;)
+    int idx = 0;
+    for (int row = 0; row < NUM_LINES; ++row)
     {
         for (int col = 0; col < ELEM_PER_LINE; ++col)
         {
-            put_int(buffer[idx]);
-            put_char(' ');
+            input[row][col] = buffer[idx];
             ++idx;
+        }
+    }
+
+    int k = 0;
+    int max_sum = ~0;
+    for (int row = 0; row < NUM_LINES; ++row)
+    {
+        int current_sum = 0;
+        for (int col = 0; col < ELEM_PER_LINE; ++col)
+        {
+            current_sum += input[row][col];
+        }
+        if (current_sum > max_sum)
+        {
+            k = row;
+        }
+    }
+
+    for (int row = 0; row < NUM_LINES; ++row)
+    {
+        for (int col = 0; col < ELEM_PER_LINE; ++col)
+        {
+            result[row][col] = input[row][col] / input[k][col];
+        }
+    }
+
+    for (int row = 0; row < NUM_LINES; ++row)
+    {
+        for (int col = 0; col < ELEM_PER_LINE; ++col)
+        {
+            put_int(result[row][col]);
+            put_char(' ');
         }
         put_char('\n');
     }
-
-    sys_exit();
 }
