7ab62c1686eb6f88646962c0d94f51d6679ff1c7
Max Dark <maxim.dark@gmail.com>
2024-12-11 11:23:54 +0300

    update test for memory_block::params::is_overlap

--- tests/mmu_tests.cxx
+++ tests/mmu_tests.cxx
@@ -10,10 +10,16 @@ int main()
     {
         range a{0, 100};
         range b{50, 150};
+        range c{50, 50};
 
+        vm::ensure(a.is_overlap(a), "!a.is_overlap(a)");
         vm::ensure(a.is_overlap(b), "!a.is_overlap(b)");
+        vm::ensure(a.is_overlap(c), "!a.is_overlap(c)");
         vm::ensure(b.is_overlap(a), "!b.is_overlap(a)");
-
+        vm::ensure(b.is_overlap(c), "!b.is_overlap(c)");
+        vm::ensure(c.is_overlap(b), "!c.is_overlap(b)");
     }
+
+    std::cout << "ok" << std::endl;
     return EXIT_SUCCESS;
 }
