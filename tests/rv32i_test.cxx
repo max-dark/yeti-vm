988570fdecf01a31022126911e5c8ecff49b5614
Max Dark <maxim.dark@gmail.com>
2024-12-15 10:43:30 +0300

    add tests for register getters

--- tests/rv32i_test.cxx
+++ tests/rv32i_test.cxx
@@ -28,3 +28,61 @@ TEST(InstructionParser, GetCode)
         }
     }
 }
+
+TEST(InstructionParser, GetRD)
+{
+    constexpr uint8_t reg_start = 7;
+    constexpr uint8_t reg_size  = 5;
+    constexpr uint8_t reg_last  = reg_start + reg_size;
+    opcode_t mask = make_mask<reg_start, reg_size>();
+    opcode_t lo = make_mask<0, reg_start - 2>();
+    opcode_t hi = make_mask<0, reg_last  + 2>();
+    OpcodeBase parser{};
+    auto& code = parser.code;
+
+    for (opcode_t i = lo; i <= hi; ++i)
+    {
+        code = i;
+        auto expected = (i & mask) >> reg_start;
+        ASSERT_EQ(parser.get_rd(), expected);
+    }
+}
+
+TEST(InstructionParser, GetRS1)
+{
+    constexpr uint8_t reg_start = 15;
+    constexpr uint8_t reg_size  =  5;
+    constexpr uint8_t reg_last  =  reg_start + reg_size;
+    constexpr opcode_t mask = make_mask<reg_start, reg_size>();
+    // FIXME: range too big
+    constexpr opcode_t lo = make_mask<0, reg_start - 2>();
+    constexpr opcode_t hi = make_mask<0, reg_last  + 2>();
+    OpcodeBase parser{};
+    auto& code = parser.code;
+
+    for (opcode_t i = lo; i <= hi; ++i)
+    {
+        code = i;
+        auto expected = (i & mask) >> reg_start;
+        ASSERT_EQ(parser.get_rs1(), expected);
+    }
+}
+
+TEST(InstructionParser, GetRS2)
+{
+    constexpr uint8_t reg_start = 20;
+    constexpr uint8_t reg_size  =  5;
+    constexpr uint8_t reg_last  =  reg_start + reg_size;
+    constexpr opcode_t mask = make_mask<reg_start, reg_size>();
+    constexpr opcode_t lo = make_mask<0, reg_start - 2>();
+    constexpr opcode_t hi = make_mask<0, reg_last  + 2>();
+    OpcodeBase parser{};
+    auto& code = parser.code;
+
+    for (opcode_t i = lo; i <= hi; ++i)
+    {
+        code = i;
+        auto expected = (i & mask) >> reg_start;
+        ASSERT_EQ(parser.get_rs2(), expected);
+    }
+}
