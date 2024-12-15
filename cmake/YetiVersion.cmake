688503ea19062bce8c78320d1b6c0b879e29353a
Max Dark <maxim.dark@gmail.com>
2024-12-15 04:23:12 +0300

    add cmake/YetiVersion.cmake - project description

---
+++ cmake/YetiVersion.cmake
@@ -0,0 +1,11 @@
+# version definitions
+
+set(YetiVersion_MAJOR 0)
+set(YetiVersion_MINOR 0)
+set(YetiVersion_PATCH 2)
+
+# Current version
+set(YetiVersion "${YetiVersion_MAJOR}.${YetiVersion_MINOR}.${YetiVersion_PATCH}")
+
+# project description
+set(YetiProject "Yeti Project / RISC-V virtual machine")
