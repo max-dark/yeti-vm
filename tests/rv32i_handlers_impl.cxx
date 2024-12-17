ca24debfd545cc1b17f00155da5a4685f38252cc
Max Dark <maxim.dark@gmail.com>
2024-12-17 21:52:11 +0300

    add test "RV32I handlers" / JumpAndLinkRegister

--- tests/rv32i_handlers_impl.cxx
+++ tests/rv32i_handlers_impl.cxx
@@ -108,4 +108,53 @@ TEST_F(RV32I_Handler_Impl, JumpAndLink)
     }
 }
 
+TEST_F(RV32I_Handler_Impl, JumpAndLinkRegister)
+{
+    MockVM mock;
+    // jalr instruction ...
+    auto impl = create<jalr>();
+
+    auto &id = impl->get_id();
+    // group / encoding / FIXME: have a-ext[0], but ignored
+    ASSERT_TRUE(id.equal(make_id(GroupId::JALR, Format::I_TYPE)));
+
+    // immediate encodes signed offset relative to RS1 value
+    // addr = rs1 + imm
+    // generates exception if <addr> not aligned to four bytes
+    // ok = <addr> % 4 !=0
+    // saves next instruction address in dest
+    // rd = pc + 4
+
+    RegId regId[] = { RegAlias::zero, RegAlias::ra, RegAlias::t0 };
+    Offset offset[] = { -8, -4, 0, +4, +8 };
+    Address pc_min = 0;
+    Address pc_max = 32;
+
+    using bits = vm::bit_tools::bits<Offset>;
+    RegId rs1 = 0;
+    for (auto pc = pc_min; pc <= pc_max; ++pc)
+    {
+        Address ptr = pc * 4;
+        for (auto o: offset)
+        {
+            auto imm = bits::to_unsigned(o);
+            for (auto rd: regId)
+            {
+                Decoder parser{ Encoder::i_type(GroupId::JALR, rd, rs1, imm, 0) };
+                auto rs1_value = ptr * rs1;
+                EXPECT_CALL(mock, get_register(rs1))
+                    .WillRepeatedly(Return(rs1_value));
+                EXPECT_CALL(mock, get_pc())
+                        .WillRepeatedly(Return(ptr));
+                EXPECT_CALL(mock, set_register(rd, ptr + 4));
+                EXPECT_CALL(mock, jump_abs(rs1_value + o));
+
+                impl->exec(&mock, &parser);
+
+                rs1 = (rs1 + 1) % vm::register_count;
+            }
+        }
+    }
+}
+
 } // namespace tests::rv32i
