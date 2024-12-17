d23ffa860ef73e0ee40975bf77451fb0f8dc3c01
Max Dark <maxim.dark@gmail.com>
2024-12-17 08:20:00 +0300

    add test "RV32I handlers" / AddUpperImmediateToPC

--- tests/rv32i_handlers_impl.cxx
+++ tests/rv32i_handlers_impl.cxx
@@ -9,6 +9,7 @@ using namespace vm::rv32i;
 
 using ::testing::_;
 using ::testing::Args;
+using ::testing::Return;
 
 using Format = vm::opcode::BaseFormat;
 
@@ -38,6 +39,31 @@ TEST_F(RV32I_Handler_Impl, LoadUpperImmediate)
         EXPECT_CALL(mock, set_register(r_id, imm));
         impl->exec(&mock, &parser);
     }
+}
+
+TEST_F(RV32I_Handler_Impl, AddUpperImmediateToPC)
+{
+    MockVM mock;
+    auto impl = create<auipc>();
+
+    ASSERT_EQ(impl->get_code_base(), Enum::AUIPC);
+    ASSERT_EQ(impl->get_type(), Format::U_TYPE);
+
+    for (uint8_t r_id = 0; r_id < vm::register_count; ++ r_id)
+    {
+        Code imm = r_id << 12u; // TODO: test with MSB = 1
+        Code code = Encoder::u_type(impl->get_code_base(), r_id, imm);
+        Decoder parser{code};
+        ASSERT_EQ(parser.get_rd(), r_id);
+        ASSERT_EQ(parser.decode_u_u(), imm);
+        ASSERT_EQ(parser.get_code(), impl->get_code_base());
+
+        EXPECT_CALL(mock, get_pc())
+            .WillRepeatedly(Return(r_id * r_id));
+        EXPECT_CALL(mock, set_register(r_id, imm + r_id * r_id));
+        impl->exec(&mock, &parser);
+    }
+
 }
 
 } // namespace tests::rv32i
