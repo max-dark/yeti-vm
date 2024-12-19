2c9c6838650de1ced975f2a58d8185131a6b8966
Max Dark <maxim.dark@gmail.com>
2024-12-19 05:47:43 +0300

    test "RV32I handlers": Int Reg-Imm / AndImmediate

--- tests/rv32i_handlers_integer_ri.cxx
+++ tests/rv32i_handlers_integer_ri.cxx
@@ -197,7 +197,19 @@ TEST_F(RV32I_Handler_RI, OrImmediate)
 
 TEST_F(RV32I_Handler_RI, AndImmediate)
 {
-    NotImplemented();
+    static constexpr Code funcA = 0b0111;
+    auto impl = create<andi>();
+    TestStep step = [](const vm::interface* impl, const TestParams* p) -> TestValues
+    {
+        TestValues r{};
+        r.code = encode(p->dest, p->src, p->data, funcA);
+        DecoderShouldReturnSameValue(r, p, funcA);
+        r.src = p->src * (p->dest ^ p->data);
+        r.dest = r.src & r.code.decode_i();
+
+        return r;
+    };
+    return commonTest(impl, funcA, step);
 }
 
 TEST_F(RV32I_Handler_RI, ShiftLeftLogicalImmediate)
