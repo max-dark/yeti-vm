66e3b95688675dae21b4c325f256416f04158bf5
Max Dark <maxim.dark@gmail.com>
2024-12-19 15:00:01 +0300

    "M" Extension tests / add integration tests

--- tests/rv32ext_m_handlers.cxx
+++ tests/rv32ext_m_handlers.cxx
@@ -12,7 +12,9 @@ using namespace vm::rv32m;
 using vm::bit_tools::bits;
 
 using ::testing::_;
+using ::testing::Expectation;
 using ::testing::Return;
+using ::testing::Sequence;
 
 using namespace tests::rv32_vm;
 
@@ -24,6 +26,10 @@ using vm::opcode::Decoder;
 using vm::opcode::Encoder;
 using Code = vm::opcode::opcode_t;
 using ExtId = Code;
+
+template<class Handler>
+concept Implementation = std::is_base_of_v<vm::interface, Handler>;
+using HandlerPtr = std::unique_ptr<vm::interface>;
 
 /**
  * "M" Extension for Integer Multiplication and Division
@@ -49,46 +55,104 @@ protected:
                 funcA, funcB
         };
     }
+
+    template<Implementation Type>
+    [[nodiscard]]
+    static auto create()
+    {
+        return std::make_unique<Type>();
+    }
+
+    static void integrationTest(vm::interface* impl, ExtId extId)
+    {
+        ASSERT_TRUE(impl->get_id().equal(expectedId(extId)));
+
+        MockVM mockVm;
+
+        RegId id_dst = 1;
+        RegId id_lhs = 2;
+        RegId id_rhs = 3;
+
+        Value res = 0;
+        Value lhs = 0;
+        Value rhs = 0;
+
+        Decoder code = encode(id_dst, id_lhs, id_rhs, extId);
+
+        Expectation get_lhs = EXPECT_CALL(mockVm, get_register(id_lhs))
+            .WillOnce(Return(lhs));
+        Expectation get_rhs = EXPECT_CALL(mockVm, get_register(id_rhs))
+            .WillOnce(Return(rhs));
+
+        EXPECT_CALL(mockVm, set_register(id_dst, _))
+            .Times(1)
+            .After(get_lhs, get_rhs);
+
+        impl->exec(&mockVm, &code);
+    }
+
+    void SetUp() override
+    {
+        //disableUnitTestWarning = true;
+    }
+protected:
+    bool disableUnitTestWarning = false;
 };
 
 TEST_F(RV32Ext_IntMath, IntMul)
 {
-    FAIL() << "Not implemented";
+    auto impl = create<mul>();
+    EXPECT_TRUE(disableUnitTestWarning) << impl->get_mnemonic() << " Unit test is not implemented";
+    return integrationTest(impl.get(), 0b0000);
 }
 
 TEST_F(RV32Ext_IntMath, IntMulH)
 {
-    FAIL() << "Not implemented";
+    auto impl = create<mulh>();
+    EXPECT_TRUE(disableUnitTestWarning) << impl->get_mnemonic() << " Unit test is not implemented";
+    return integrationTest(impl.get(), 0b0001);
 }
 
 TEST_F(RV32Ext_IntMath, IntMulHSU)
 {
-    FAIL() << "Not implemented";
+    auto impl = create<mulhsu>();
+    EXPECT_TRUE(disableUnitTestWarning) << impl->get_mnemonic() << " Unit test is not implemented";
+    return integrationTest(impl.get(), 0b0010);
 }
 
 TEST_F(RV32Ext_IntMath, IntMulHU)
 {
-    FAIL() << "Not implemented";
+    auto impl = create<mulhu>();
+    EXPECT_TRUE(disableUnitTestWarning) << impl->get_mnemonic() << " Unit test is not implemented";
+    return integrationTest(impl.get(), 0b0011);
 }
 
 TEST_F(RV32Ext_IntMath, IntDiv)
 {
-    FAIL() << "Not implemented";
+    auto impl = create<vm::rv32m::div>();
+    EXPECT_TRUE(disableUnitTestWarning) << impl->get_mnemonic() << " Unit test is not implemented";
+    return integrationTest(impl.get(), 0b0100);
 }
 
 TEST_F(RV32Ext_IntMath, IntDivU)
 {
-    FAIL() << "Not implemented";
+    auto impl = create<divu>();
+    EXPECT_TRUE(disableUnitTestWarning) << impl->get_mnemonic() << " Unit test is not implemented";
+    return integrationTest(impl.get(), 0b0101);
 }
 
 TEST_F(RV32Ext_IntMath, IntRem)
 {
-    FAIL() << "Not implemented";
+    auto impl = create<rem>();
+    EXPECT_TRUE(disableUnitTestWarning) << impl->get_mnemonic() << " Unit test is not implemented";
+    return integrationTest(impl.get(), 0b0110);
 }
 
 TEST_F(RV32Ext_IntMath, IntRemU)
 {
-    FAIL() << "Not implemented";
+    auto impl = create<remu>();
+    EXPECT_TRUE(disableUnitTestWarning) << impl->get_mnemonic() << " Unit test is not implemented";
+    return integrationTest(impl.get(), 0b0111);
 }
 
 } // namespace tests::rv32m
