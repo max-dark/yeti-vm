5652eee2835e5290caefa327c5afe987bbbc82bb
Max Dark <maxim.dark@gmail.com>
2024-12-19 08:44:50 +0300

    test "RV32I handlers": Int Reg-Reg(OP / R-type) / add shared code

--- tests/rv32i_handlers_integer_rr.cxx
+++ tests/rv32i_handlers_integer_rr.cxx
@@ -15,6 +15,52 @@ class RV32I_Handler_RR
         : public RV32I_Handler
 {
 protected:
+    static Decoder encode(RegId dest, RegId lhs, RegId rhs, Code funcA, Code funcB)
+    {
+        Code code = Encoder::r_type(
+                GroupId::OP
+                , dest, lhs, rhs
+                , funcA, funcB
+                );
+        return Decoder{ code };
+    }
+
+    static vm::InstructionId expectedId(Code funcA, Code funcB)
+    {
+        return make_id(GroupId::OP_IMM, Format::R_TYPE, funcA, funcB);
+    }
+
+    using TestStep = std::function<Code(Code lhs, Code rhs)>;
+    static void commonTest(const vm::interface* impl, Code funcA, Code funcB, const TestStep& step)
+    {
+        ASSERT_TRUE(impl->get_id().equal(expectedId(funcA, funcA)));
+
+        for (RegId dest = 0; dest < vm::register_count; ++dest)
+        {
+            for (Offset values: {-8, -4, 0, +4, +8})
+            {
+                MockVM mockVm;
+
+                RegId id_dst = 0;
+                RegId id_lhs = 0;
+                RegId id_rhs = 0;
+
+                Code lhs = 0;
+                Code rhs = 0;
+                Code res = step(lhs, rhs);
+
+                Decoder code = encode(id_dst, id_lhs, id_rhs, funcA, funcB);
+
+                EXPECT_CALL(mockVm, get_register(id_lhs))
+                    .WillRepeatedly(Return(lhs));
+                EXPECT_CALL(mockVm, get_register(id_rhs))
+                        .WillRepeatedly(Return(rhs));
+                EXPECT_CALL(mockVm, set_register(id_dst, res));
+
+                impl->exec(&mockVm, &code);
+            }
+        }
+    }
 };
 
 TEST_F(RV32I_Handler_RR, Add)
