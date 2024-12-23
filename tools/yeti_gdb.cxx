5827eacdef4f127abab811153c21c4173a0023f0
Max Dark <maxim.dark@gmail.com>
2024-12-23 05:39:29 +0300

    Play with GDB remote protocol

--- tools/yeti_gdb.cxx
+++ tools/yeti_gdb.cxx
@@ -50,11 +50,13 @@ $qAttached#8f
  */
 
 // https://sourceware.org/gdb/current/onlinedocs/gdb.html/Packets.html#Packets
+// https://ftp.gnu.org/old-gnu/Manuals/gdb/html_node/gdb_129.html
 namespace gdb_remote
 {
-    enum Standard: char
+    enum Command: char
     {
-        HALT_CAUSE = '?',
+        /// Indicate the reason the target halted. The reply is the same as for step and continue.
+        LAST_SIGNAL = '?',
         CONTINUE_c = 'c', // continue
         CONTINUE_C = 'C', // continue with signal
         STEP_s = 's', // single step
@@ -73,18 +75,28 @@ namespace gdb_remote
         LOAD_BIN = 'X',
         BREAK_CLR = 'z',
         BREAK_SET = 'Z',
+        /// H<cmd><thread> set current thread for command
         THREAD_SET = 'H',
     };
 
     enum Protocol: char
     {
+        /// command accepted
         GDB_ACK = '+',
+        /// command rejected
         GDB_NAK = '-',
+        /// data start mark
         GDB_BEG = '$',
+        /// data end mark
         GDB_END = '#',
+        /// next char is escaped and XOR-ed with 0x20
         GDB_ESC = '}',
+        /// encode/decode escaped char: c = c ^ 0x20
         GDB_XOR = 0x20, // just ' ' - space
-        GDB_RLE = '*',
+        /// RLE encoded, next char - length
+        GDB_RLE_MARK = '*',
+        /// rle_length = c - 0x28
+        GDB_RLE_LENGTH = 0x28,
     };
 } // namespace gdb_remote
 
@@ -96,8 +108,10 @@ int main(int argc, char ** argv)
 
     tcp::socket client(ctx);
     server.accept(client);
+    std::cout << "wait for connection" << std::endl;
     try
     {
+        tcp::iostream stream;
         bool run = true;
         auto make_answer = [](const std::string& data)
         {
@@ -110,6 +124,7 @@ int main(int argc, char ** argv)
         int state = 0;
         do
         {
+            ++state;
             using namespace gdb_remote;
             std::string input, output;
 
@@ -154,13 +169,13 @@ int main(int argc, char ** argv)
                         output = char(GDB_ACK) + make_answer(""); // NOLINT(bugprone-branch-clone)
                     else if (cmd.starts_with("vMustReplyEmpty"))
                         output = char(GDB_ACK) + make_answer("");
-                    else if (cmd.starts_with(">vKill"))
+                    else if (cmd.starts_with("vKill"))
                         output = char(GDB_ACK) + make_answer("OK");
                     else
                         output = char(GDB_NAK) + make_answer("E01");
                     break;
                 }
-                case HALT_CAUSE:
+                case LAST_SIGNAL:
                 {
                     output = char(GDB_ACK) + make_answer("S05");
                     break;
